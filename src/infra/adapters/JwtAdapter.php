69cdb9d9539d330ff289c42f72cc5d2f07a367bd
Douglas Volcato <douglasvolcato@gmail.com>
2023-12-09 00:33:27 -0300

    feat: implement token generation

--- src/infra/adapters/JwtAdapter.php
+++ src/infra/adapters/JwtAdapter.php
@@ -2,39 +2,26 @@
 
 namespace PetAdoption\infra\adapters;
 
-use Lcobucci\JWT\Builder;
-use Lcobucci\JWT\Parser;
-use Lcobucci\JWT\ValidationData;
-use Lcobucci\JWT\Signer\Hmac\Sha256;
-use Lcobucci\JWT\Signer\Key;
 use PetAdoption\domain\protocols\utils\TokenDecrypterInterface;
 use PetAdoption\domain\protocols\utils\TokenGeneratorInterface;
+use Firebase\JWT\JWT;
 
 class JwtAdapter implements TokenGeneratorInterface, TokenDecrypterInterface
 {
-    public function generateToken($content, $secret): string
+    public function generateToken(object $content, string $secret): string
     {
-        $builder = new Builder();
-        $token = $builder
-        ->withClaim('your_custom_claim', $content)
-        ->getToken(new Sha256(), new Key($secret));
+        $token = JWT::encode(
+            (array)$content,
+            $secret,
+            'HS256'
+        );
 
-        return $token->__toString();
+        return $token;
     }
 
-    public function decryptToken($token, $secret): object|null
+    public function decryptToken(string $jwt, string $secret): object|null
     {
-        try {
-            $parsedToken = (new Parser())->parse((string)$token);
-            $data = new ValidationData();
-
-            if ($parsedToken->validate($data) && $parsedToken->verify(new Sha256(), $secret)) {
-                return $parsedToken->getClaim('your_custom_claim');
-            }
-
-            return null;
-        } catch (\Exception $exception) {
-            return null;
-        }
+        $decoded = JWT::decode($jwt, $secret, (object)['algorithms' => ['HS256']]);
+        return (object) $decoded->data;
     }
 }
