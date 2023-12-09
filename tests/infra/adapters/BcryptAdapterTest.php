f7043524677b372591e95fc9b99be1e5b801c008
Douglas Volcato <douglasvolcato@gmail.com>
2023-12-09 18:52:47 -0300

    test: ensure password validation is working

--- tests/infra/adapters/BcryptAdapterTest.php
+++ tests/infra/adapters/BcryptAdapterTest.php
@@ -42,7 +42,6 @@ class BcryptAdapterTest extends TestCase
                 })
             )
             ->andReturn($this->fakeData->password());
-
         $bcryptAdapterMock->hash($password);
     }
 
@@ -57,7 +56,6 @@ class BcryptAdapterTest extends TestCase
         $bcryptAdapterMock = Mockery::mock(BcryptAdapter::class)->makePartial();
         $bcryptAdapterMock->shouldReceive('passwordHash')
             ->andReturn($hashedPassword);
-
         $output = $bcryptAdapterMock->hash($password);
         $this->assertEquals($hashedPassword, $output);
     }
@@ -73,5 +71,72 @@ class BcryptAdapterTest extends TestCase
             ->andThrow(new Exception());
         $this->expectException(Exception::class);
         $bcryptAdapterMock->hash($password);
+    }
+
+    /**
+     * @covers \PetAdoptionTest\infra\adapters\BcryptAdapter::validate
+     */
+    public function testShouldCallValidateWithCorrectValues()
+    {
+        $password = $this->fakeData->password();
+        $hashedPassword = $this->fakeData->password();
+
+        $bcryptAdapterMock = Mockery::mock(BcryptAdapter::class)->makePartial();
+        $bcryptAdapterMock->shouldReceive('passwordVerify')
+            ->with(
+                Mockery::on(function ($arg) use ($password) {
+                    $this->assertEquals($password, $arg);
+                    return true;
+                }),
+                Mockery::on(function ($arg) use ($hashedPassword) {
+                    $this->assertEquals($hashedPassword, $arg);
+                    return true;
+                })
+            )->andReturn(true);
+        $bcryptAdapterMock->validate($password, $hashedPassword);
+    }
+
+    /**
+     * @covers \PetAdoptionTest\infra\adapters\BcryptAdapter::validate
+     */
+    public function testShouldReturnTrueIfValidateReturnsTrue()
+    {
+        $bcryptAdapterMock = Mockery::mock(BcryptAdapter::class)->makePartial();
+        $bcryptAdapterMock->shouldReceive('passwordVerify')->andReturn(true);
+        $output = $bcryptAdapterMock->validate(
+            $this->fakeData->password(),
+            $this->fakeData->password()
+        );
+        $this->assertTrue($output);
+    }
+
+    /**
+     * @covers \PetAdoptionTest\infra\adapters\BcryptAdapter::validate
+     */
+    public function testShouldReturnFalseIfValidateReturnsFalse()
+    {
+        $bcryptAdapterMock = Mockery::mock(BcryptAdapter::class)->makePartial();
+        $bcryptAdapterMock->shouldReceive('passwordVerify')->andReturn(false);
+        $output = $bcryptAdapterMock->validate(
+            $this->fakeData->password(),
+            $this->fakeData->password()
+        );
+        $this->assertFalse($output);
+    }
+
+    /**
+     * @covers \PetAdoptionTest\infra\adapters\BcryptAdapter::validate
+     */
+    public function testShouldThrowIfValidateThrows()
+    {
+        $bcryptAdapterMock = Mockery::mock(BcryptAdapter::class)
+            ->makePartial();
+        $bcryptAdapterMock->shouldReceive('passwordVerify')
+            ->andThrow(new Exception());
+        $this->expectException(Exception::class);
+        $bcryptAdapterMock->validate(
+            $this->fakeData->password(),
+            $this->fakeData->password()
+        );
     }
 }
