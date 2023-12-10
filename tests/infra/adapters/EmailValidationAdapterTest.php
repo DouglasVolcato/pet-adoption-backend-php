00447f5c6349d8a590b4efa554c6708cdeb33969
Douglas Volcato <douglasvolcato@gmail.com>
2023-12-10 13:01:57 -0300

    test: ensure email validation is working

---
+++ tests/infra/adapters/EmailValidationAdapterTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace PetAdoptionTest\infra\adapters;
+
+use PetAdoption\infra\adapters\EmailValidationAdapter;
+use Egulias\EmailValidator\Validation\RFCValidation;
+use Exception;
+use PetAdoptionTest\utils\FakeData;
+use Mockery\LegacyMockInterface;
+use PHPUnit\Framework\TestCase;
+use Mockery;
+
+class EmailValidationAdapterTest extends TestCase
+{
+    private EmailValidationAdapter $sut;
+    private FakeData $fakeData;
+    private LegacyMockInterface $emailValidatorMock;
+
+    protected function setUp(): void
+    {
+        $this->emailValidatorMock = Mockery::mock('overload:Egulias\EmailValidator\EmailValidator');
+        $this->fakeData = FakeData::getInstance();
+        $this->sut = new EmailValidationAdapter();
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    /**
+     * @covers \PetAdoption\infra\adapters\EmailValidationAdapter::isEmail
+     */
+    public function testShouldCallEmailValidatorWithCorrectValues()
+    {
+        $email = $this->fakeData->email();
+        $this->emailValidatorMock->shouldReceive('isValid')->with(
+            Mockery::on(
+                function ($arg) use ($email) {
+                    $this->assertEquals($email, $arg);
+                    return true;
+                }
+            ),
+            Mockery::on(
+                function ($arg) {
+                    $this->assertEquals(new RFCValidation(), $arg);
+                    return true;
+                }
+            )
+        )->andReturn(true);
+        $this->sut->isEmail($email);
+    }
+
+    /**
+     * @covers \PetAdoption\infra\adapters\EmailValidationAdapter::isEmail
+     */
+    public function testShouldReturnTrueIfEmailValidatorReturnsTrue()
+    {
+        $this->emailValidatorMock->shouldReceive('isValid')->andReturn(true);
+        $result = $this->sut->isEmail($this->fakeData->email());
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @covers \PetAdoption\infra\adapters\EmailValidationAdapter::isEmail
+     */
+    public function testShouldReturnFalseIfEmailValidatorReturnsFalse(){
+        $this->emailValidatorMock->shouldReceive('isValid')->andReturn(false);
+        $result = $this->sut->isEmail($this->fakeData->email());
+        $this->assertFalse($result);
+    }
+
+    /**
+     * @covers \PetAdoption\infra\adapters\EmailValidationAdapter::isEmail
+     */
+    public function testShouldThrowIfEmailValidatorThrows(){
+        $this->emailValidatorMock->shouldReceive('isValid')->andThrow(new Exception());
+        $this->expectException(Exception::class);
+        $this->sut->isEmail($this->fakeData->email());
+    }
+}
